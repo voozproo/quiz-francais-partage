293ddf99a41b164f7ab88dcf1daeaeec1ee91b71
voozproo <voozproo@gmail.com>
2025-06-13 14:20:05 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,15 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/quiz-francais-partage/", // 👈 VERY IMPORTANT for GitHub Pages
+
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
